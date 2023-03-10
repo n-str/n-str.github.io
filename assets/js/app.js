4ef7b06ae7f7d6e0cfc1c56e0586e0dc452dee21
Nataliia Strubitska <122638806+n-str@users.noreply.github.com>
2023-03-10 13:01:47 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,15 @@ jQuery(document).ready(function(){
 		$("#preloader").fadeOut(1000);
 	});
 
+	$('.marquee').marquee({
+		duration:         10000,
+		gap:              12,
+		loop:             -1,
+		delayBeforeStart: 0,
+		direction:        'left',
+		pauseOnHover:     false,
+		duplicated:       true
+	});
   
   
 });
